53c5baa2641ef9cb001f513547f098ffa7e10838
Siong Ong <siong1987@gmail.com>
2022-11-25 18:55:56 +0100

    allow another getSwapTickArrays to be exposed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import Decimal from './utils/decimal';
+import Decimal from "./utils/decimal";
 
-export * from './types/index';
-export { IDL, clmmpool } from './idls/clmmpool';
-export { computeSwap, TickMath } from './math';
-export { PDAUtil, TickUtil } from './utils';
+export * from "./types/index";
+export { IDL, clmmpool } from "./idls/clmmpool";
+export { computeSwap, getSwapTickArrays, TickMath } from "./math";
+export { PDAUtil, TickUtil } from "./utils";
 
 Decimal.set({ precision: 40, toExpPos: 40, toExpNeg: -20, rounding: 1 });
