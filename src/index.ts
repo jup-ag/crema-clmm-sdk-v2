29fbc069ca5dd4ec7a929947550a240604a0e3e3
Siong Ong <siong1987@gmail.com>
2022-11-26 17:28:52 +0100

    update again.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Decimal from "./utils/decimal";
 
 export * from "./types/index";
 export { IDL, clmmpool } from "./idls/clmmpool";
-export { computeSwap, getSwapTickArrays, TickMath } from "./math";
+export { computeSwap, TickMath } from "./math";
 export { PDAUtil, TickUtil } from "./utils";
 
 Decimal.set({ precision: 40, toExpPos: 40, toExpNeg: -20, rounding: 1 });
